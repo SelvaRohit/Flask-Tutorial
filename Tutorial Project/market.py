7ed1e39c29528cb153df3074d8320a4c10a8419e
Selva <selvakaruppasamy2018@gmail.com>
2025-11-20 00:31:51 +0530

    Learning DB modules, how to add details in db table.

--- Tutorial Project/market.py
+++ Tutorial Project/market.py
@@ -1,6 +1,19 @@
 from flask import Flask ,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+# URI - Unified Resource Identifier
+db=SQLAlchemy(app=app)
+
+class Item(db.Model):
+
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(length=30),nullable=False,unique=True)
+    price=db.Column(db.Integer(),nullable=False)
+    barcode=db.Column(db.String(length=12),nullable=False,unique=True)
+    description=db.Column(db.String(length=10240),nullable=False,unique=True)
+
 print("hello")
 @app.route("/")
 @app.route("/home")
@@ -17,3 +30,8 @@ def market_page():
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
     return render_template('market.html',items=items)
+if __name__=='__main__':
+    # from market import app, db
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
